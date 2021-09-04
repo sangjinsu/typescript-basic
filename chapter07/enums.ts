25d7c2a4c1c9d0141048641b0d485475f868d361
jinsuSang <climbplant39@kakao.com>
2021-09-04 20:51:45 +0900

    enum

---
+++ chapter07/enums.ts
@@ -0,0 +1,32 @@
+// 숫자형 enum
+enum Shoes {
+  Nike,
+  Adidas,
+  Fila,
+  NewBalance = 10,
+}
+
+const mySheos = Shoes.Nike // 0
+Shoes.NewBalance // 10
+
+// 문장형 enum
+enum Car {
+  Audi = 'Audi',
+  Honda = 'Honda',
+  Kia = 'Kia',
+}
+
+enum Answer {
+  Yes = '?Yes',
+  No = 'No',
+}
+function ask(answer: Answer) {
+  if (answer === Answer.Yes) {
+    console.log('correct')
+  }
+  if (answer === Answer.No) {
+    console.log('wrong')
+  }
+}
+
+ask(Answer.Yes)
